ea6c72634d2f72472b6050c548ad5751f17914e9
JYuan <31184164+WaterSeeding@users.noreply.github.com>
2023-05-31 17:51:54 +0800

    添加gui

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
 import "./app.css";
 import * as Cesium from "cesium";
 import { viewer } from "./main";
+import * as dat from "dat.gui";
+
+const gui = new dat.GUI({ name: "Cesium GUI", autoPlace: true });
+gui.domElement.id = "gui";
+
+let guiTarget: { [key: string]: any } = {
+  type: "cesium",
+};
+
+guiTarget["Add a Cesium_Air Model"] = () => {
+  createModel("./static/CesiumAir/Cesium_Air.glb", 5000.0);
+};
+
+gui.add(guiTarget, "Add a Cesium_Air Model");
 
 const createModel = (url: string, height: number) => {
   viewer.entities.removeAll();
@@ -18,10 +32,12 @@ const createModel = (url: string, height: number) => {
     position,
     hpr
   );
+  const orientationProperty = new Cesium.ConstantProperty(orientation);
 
   const entity = viewer.entities.add({
     name: url,
     position: position,
+    orientation: orientationProperty,
     model: {
       uri: url,
       minimumPixelSize: 128,
@@ -30,7 +46,3 @@ const createModel = (url: string, height: number) => {
   });
   viewer.trackedEntity = entity;
 };
-
-setTimeout(() => {
-  createModel("./static/CesiumAir/Cesium_Air.glb", 5000.0);
-}, 5000)
